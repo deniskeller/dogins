263f8b946fb58d423341bb215b542f175ff9f547
deniskeller <dentean@mail.ru>
2023-03-06 01:47:19 +0400

    ftr

--- src/components/landing/footer/Footer/Footer.tsx
+++ src/components/landing/footer/Footer/Footer.tsx
@@ -1,68 +1,101 @@
+import { BaseIcon } from '@base/index';
+import { ALL_ICONS } from '@constants/icons';
+import { Logo } from '@content/landing/index';
 import Link from 'next/link';
-import styles from './Footer.module.scss';
-// import { BaseIcon } from '@base/index';
+import s from './Footer.module.scss';
 
-const links = [
-	[
-		{ title: 'Terms & Conditions', href: '/terms_and_conditions' },
-		{ title: 'Cookie Policy', href: '/cookie_policy' },
-		{ title: 'Privacy Policy', href: '/privacy_policy' },
-		{ title: 'Complaints Policy', href: '/complaints_policy' },
-	],
-	[
-		{ title: 'About Us', href: '/about_us' },
-		{ title: 'Contact Us', href: '/contact_us' },
-		{ title: 'News Room', href: '/news_room' },
-		{ title: 'Portfolio', href: '/portfolio' },
-		{ title: 'Financial Data', href: '/coming_soon' },
-	],
+interface list {
+	name: string;
+	href: string;
+}
+
+interface Links {
+	title: string;
+	list: list[];
+}
+
+const Links: Links[] = [
+	{
+		title: 'Products',
+		list: [
+			{ name: 'Compliance', href: '/compliance' },
+			{ name: 'Licensing', href: '/licensing' },
+			{ name: 'Investments', href: '/investments' },
+		],
+	},
+	{
+		title: 'Company',
+		list: [
+			{ name: 'About', href: '/about' },
+			{ name: 'News&Insights', href: '/new' },
+			{ name: 'Careers', href: '/careers' },
+			{ name: 'Eco', href: '/eco' },
+		],
+	},
+	{
+		title: 'Resources',
+		list: [
+			{ name: 'Contacts', href: '/Contacts' },
+			{ name: 'Personal Data Protection', href: '/personal-data-protection' },
+			{ name: 'Services Agreement', href: '/services-agreement' },
+			{ name: 'Intellectual Property', href: '/intellectual-property' },
+		],
+	},
+	{
+		title: 'Pricing',
+		list: [
+			{ name: 'Financial licensing', href: '/financial-licensing' },
+			{ name: 'Compliance software', href: '/compliance-software' },
+			{ name: 'Payment software', href: '/payment-software' },
+			{ name: 'Support services', href: '/support-services' },
+		],
+	},
+	{
+		title: 'Website rules',
+		list: [
+			{ name: 'Privacy Policy', href: '/privacy-policy' },
+			{ name: 'Cookies Policy', href: '/cookies-policy' },
+			{ name: 'Terms of Use', href: '/termsof-use' },
+		],
+	},
 ];
 
 const Footer = () => {
 	return (
 		<>
-			<div className={styles.Container}>
-				<div className={styles.Footer}>
-					<div className={styles.Footer_Content}>
-						<div className={styles.Footer_Content_Title}>
-							The wealth platform for FinTech
+			<div className={s.Wrapper}>
+				<div className={s.Container}>
+					<div className={s.Footer}>
+						<div className={s.Footer_LogoSocial}>
+							<Logo className={s.Footer_LogoSocial_Logo} />
+
+							<div className={s.Footer_LogoSocial_Socials}>
+								<div className={s.Footer_LogoSocial_Socials_Items}>
+									<BaseIcon icon={ALL_ICONS.FACEBOOK} viewBox='0 0 23 24' />
+									<BaseIcon icon={ALL_ICONS.LINKEDIN} viewBox='0 0 23 24' />
+									<BaseIcon icon={ALL_ICONS.TWITTER} viewBox='0 0 23 24' />
+								</div>
+
+								<div className={s.Footer_LogoSocial_Socials_Copywriting}>
+									<p>© 2023 Doqins & Partners Limited</p>
+								</div>
+							</div>
 						</div>
 
-						<div className={styles.Footer_Content_Description}>
-							<div className={styles.Footer_Content_Description_Text}>
-								<span>
-									<p>
-										Manhattan VC Holding is registered in the United States of
-										America, in the State of New York and is based in NYC
-										Manhattan. We are registered for Anti-Money-Laundering
-										purposes only. We are not intended to be a substitute for
-										legal, tax or financial advice. Operating an investment
-										vehicle may be regulated in your country. We do not assist
-										with your regulation as an investment manager or by
-										conducting any other regulated activity.
-									</p>
-								</span>
-
-								<span>
-									<p>
-										Manhattan VC Holding is a trading name of Manhattan VC
-										Holding LLC, a limited liability company incorporated in the
-										United States of America (35-2753010) with its Business
-										Office address at 1330 6th Avenue, New York, Manhattan,
-										10019, United States of America.
-									</p>
-								</span>
-							</div>
-
-							<ul className={styles.Footer_Content_Navbar}>
-								{links?.map((link, index) => {
+						<div className={s.Footer_NavbarPartners}>
+							<ul className={s.Footer_NavbarPartners_Navbar}>
+								{Links?.map((link, index) => {
 									return (
-										<li className={styles.Link} key={index}>
-											<ul>
-												{link?.map((el, i) => {
+										<li className={s.Link} key={index}>
+											<div className={s.Link_Title}>
+												<span>{link.title}</span>
+											</div>
+
+											<ul className={s.Link_List}>
+												{link.list?.map((el, i) => {
 													return (
-														<li key={i}>
-															<Link href={el.href}>{el.title}</Link>
+														<li key={i} className={s.Link_List_Item}>
+															<Link href={el.href}>{el.name}</Link>
 														</li>
 													);
 												})}
@@ -71,18 +104,14 @@ const Footer = () => {
 									);
 								})}
 							</ul>
+
+							<BaseIcon
+								icon={ALL_ICONS.PARTNERS}
+								viewBox='0 0 234 55'
+								className={s.Footer_NavbarPartners_Partners}
+							/>
 						</div>
 					</div>
-
-					{/* <Logo className={styles.Footer_Logo} /> */}
-				</div>
-
-				<div className={styles.Footer_Copyright}>
-					<p>
-						All Rights Reserved. TM and © 2022 Manhattan VC Holding LLC. <br />
-						1330 6th Avenue, New York, Manhattan, 10019, United States of
-						America.
-					</p>
 				</div>
 			</div>
 		</>
