c3da43ee4a308854a7d4316e9874573b6925f851
deniskeller <dentean@mail.ru>
2023-04-19 15:03:15 +0400

    documents ref

--- src/layouts/Default/Default.tsx
+++ src/layouts/Default/Default.tsx
@@ -34,7 +34,7 @@ const Default: React.FC<Props> = ({ children, type }) => {
 	return (
 		<div
 			className={styles.Wrapper}
-			style={{ background: type == 'white' ? '#ffffff' : 'transparent' }}
+			style={{ background: type == 'white' ? '#F4F3F3' : 'transparent' }}
 		>
 			<Navbar fixed={isVisible} type={type} />
 
