e4b924b3b376a2f1976111a6f6736411d0542e20
deniskeller <dentean@mail.ru>
2023-04-28 17:03:06 +0400

    HongKong page

---
+++ pages/licensing/hong-kong.tsx
@@ -0,0 +1,12 @@
+import { Country } from '@layouts/index';
+import HongKong from '@view/landing/Countries/HongKong';
+
+const GermanyPage = () => {
+	return (
+		<Country type='white'>
+			<HongKong />
+		</Country>
+	);
+};
+
+export default GermanyPage;
