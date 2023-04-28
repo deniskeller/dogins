27bf474cf51b256c72b2d72de630ca75f26776a2
deniskeller <dentean@mail.ru>
2023-04-28 17:07:51 +0400

    Ireland page

---
+++ pages/licensing/ireland.tsx
@@ -0,0 +1,12 @@
+import { Country } from '@layouts/index';
+import Ireland from '@view/landing/Countries/Ireland';
+
+const GermanyPage = () => {
+	return (
+		<Country type='white'>
+			<Ireland />
+		</Country>
+	);
+};
+
+export default GermanyPage;
